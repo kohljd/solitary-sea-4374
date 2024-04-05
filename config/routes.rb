2537d1d323ec0c90ea980c1dee13aaaddd4ca9dc
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-05 11:04:41 -0500

    comment path names

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'search', to: 'search#index'
 end
+
+  #  Prefix Verb   URI Pattern           Controller#Action
+  #    root GET    /                     welcome#index
+  #  search GET    /search(.:format)     search#index
